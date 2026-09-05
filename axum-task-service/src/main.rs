3b3f6c37a0a47c3739f95e306311c095f9379626
Мария <fiz.yacyna@bsu.by>
2026-09-05 19:15:39 +0300

    Add test file, update Cargo.toml and include main module

--- axum-task-service/src/main.rs
+++ axum-task-service/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+mod handlers;
+mod structs;
+
+use dotenvy::dotenv;
+use handlers::routes::app;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let app = app().await?;
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
