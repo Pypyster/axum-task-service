ba81f4b9a9fa50625a0f11e8b55f1ff6603cf35f
Мария <fiz.yacyna@bsu.by>
2026-09-08 22:15:30 +0300

    Add main file

---
+++ axum-task-service/src/main.rs
@@ -0,0 +1,24 @@
+mod db;
+mod errors;
+mod handlers;
+mod repository;
+mod service;
+mod structs;
+
+use dotenvy::dotenv;
+use handlers::routes::app;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let app = app().await?;
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
